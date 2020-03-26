50f25dfc57cb8a9130b29700809fc50b55a92281
arthurchang09 <58202972+arthurchang09@users.noreply.github.com>
2020-03-26 14:06:30 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -63,8 +63,7 @@ def ml_loop():
                     new_x=-new_x
                 elif new_x>200:
                     new_x=400-new_x
-            #new_x-=25
-            print(new_x)
+            new_x-=25
             if new_x>platform_x :
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
             elif new_x<=platform_x:
