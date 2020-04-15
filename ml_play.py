fc3567fd3ff73a1e08805ba5754fc6a3d0bd4b73
arthurchang09 <58202972+arthurchang09@users.noreply.github.com>
2020-04-15 22:34:17 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -97,14 +97,14 @@ def ml_loop():
                     comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
                 elif platform_x+30<newp:
                     comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            """
+           
             else:
                 newp=up(ball_x,ball_y,Vx,scene_info)
                 if platform_x+10>newp:
                     comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
                 elif platform_x+30<newp:
                     comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            """
+            
             pre_Ball_x=ball_x
             pre_Ball_y=ball_y
 def down(x,y,Vx,scene_info):
@@ -134,8 +134,11 @@ def down(x,y,Vx,scene_info):
                     Vx=-Vx
                     break
     return x
+
 def up(x,y,Vx,scene_info):
-    while y>0:
+    while 1:
+        if y<0:
+            break
         if Vx>0:
             x+=7
             y-=7
